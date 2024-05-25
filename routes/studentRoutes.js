2fc4d8be349cd06766ef2831efbc590a612e3006
Megatrlynn <lynnslyna@gmail.com>
2024-05-25 07:23:41 +0200

    Initial commit

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/studentController');
 
+// Root path message
+router.get('/', (req, res) => {
+    res.send('API is working!');
+});
+
 router.post('/students', studentController.createStudent);
 router.get('/students', studentController.getStudents);
 router.get('/students/:id', studentController.getStudentById);
@@ -9,3 +14,4 @@ router.put('/students/:id', studentController.updateStudent);
 router.delete('/students/:id', studentController.deleteStudent);
 
 module.exports = router;
+
